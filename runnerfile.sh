46f91d88e431fa0f8f6ff220ae718bac5fe1b3cd
Jacob Zimmerman <zimmerman.jake@gmail.com>
2016-07-10 17:10:56 -0700

    Run shellcheck only on shell files
    Before, if there were any stray files (like tags files) in any of the
    folders, they would be shellcheck'ed. Now, we only check files which we
    are sure are shell files.

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,6 +1,29 @@
-runner_default_task="shellcheck"
-source_files=(bin/* src/*)
+#!/usr/bin/env bash
 
-task_shellcheck() {
-    runner_run shellcheck --exclude=SC2155 "${source_files[@]}"
+shopt -s globstar
+
+get_shell_files() {
+  echo "Finding shell files..." 1>&2
+
+  for file in ./bin/**/* ./src/**/*; do
+    # check for .sh ending
+    if [[ "$file" =~ .sh$ ]]; then
+      # Also report to stderr so we can get a log
+      echo "$file" | tee /dev/fd/2
+    # check if has bash in shebang on first line, and print if so
+    elif [ -f "$file" ]; then
+      # Also report to stderr so we can get a log
+      awk '/#!\/.*bash/ && NR < 2 { print FILENAME; }' "$file" | tee /dev/fd/2
+    fi
+  done
+
+  echo "Done finding shell files." 1>&2
+}
+
+get_files_and_check() {
+  get_shell_files | xargs shellcheck --exclude=SC2155
+}
+
+task_default() {
+  get_files_and_check
 }
